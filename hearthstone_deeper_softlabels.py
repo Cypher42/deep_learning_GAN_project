2dfe6621271475d76ad82b77bcd1c97f8ac6b62f
Nina Valerie Wiedemann <ninawiedemann@83adba3f.funky.uni-osnabrueck.de>
2017-07-04 13:37:55 +0200

    reuse vars for D

--- hearthstone_deeper_softlabels.py
+++ hearthstone_deeper_softlabels.py
@@ -7,15 +7,18 @@ from DataProcessorr import decode
 import os
 import csv
 import progressbar
+from tensorflow.contrib.framework import get_variables
 
 from DataProcessorr import decode_line_light
+
+sess = tf.InteractiveSession()
 
 mb_size = 128
 Z_dim = 10
 
 global train
-global sess
-sess = None
+# global sess
+#sess = None
 
 train = False
 
@@ -71,7 +74,7 @@ def xavier_init(size):
 
 def soft_labels(size, real):
     if real:
-        return tf.random_uniform(size, minval = 0.8, maxval = 1, dtype = tf.float32)
+        return tf.random_uniform(size, minval = 0.8, maxval = 1.2, dtype = tf.float32)
     else:
         return tf.random_uniform(size, minval = 0, maxval = 0.2, dtype = tf.float32)
 
@@ -88,90 +91,109 @@ def sample_Z(m, n):
 
 def generator(z):
     global sess
-    layer = tf.layers.dense(z, 11, activation = activation_function)
-    layer = tf.layers.dropout(layer,rate=0.4,training=train)
-    layer = tf.layers.batch_normalization(layer)
-    #layer = tf.layers.dense(layer, 33, activation = tf.nn.relu)
-    #layer = tf.layers.dropout(layer,rate=0.4,training=train)
-    #layer = tf.layers.batch_normalization(layer)
-    layer = tf.layers.dense(layer, 268, activation = activation_function)
-    layer = tf.layers.dropout(layer,rate=0.4,training=train)
-    layer = tf.layers.batch_normalization(layer)
-    #layer = tf.layers.dense(layer, 132, activation = tf.nn.relu)
-    #layer = tf.layers.dropout(layer,rate=0.4,training=train)
-    #layer = tf.layers.batch_normalization(layer)
-    G_prob = tf.layers.dense(layer, 134, activation = tf.nn.sigmoid)
-
+    with tf.variable_scope("generator") as scope:
+        layer = tf.layers.dense(z, 11, activation = activation_function)
+        tf.summary.histogram("g_1_layer", layer)
+        layer = tf.layers.dropout(layer,rate=0.4,training=train)
+        layer = tf.layers.batch_normalization(layer)
+        #layer = tf.layers.dense(layer, 33, activation = tf.nn.relu)
+        #layer = tf.layers.dropout(layer,rate=0.4,training=train)
+        #layer = tf.layers.batch_normalization(layer)
+        layer = tf.layers.dense(layer, 268, activation = activation_function)
+        tf.summary.histogram("g_2_layer", layer)
+        layer = tf.layers.dropout(layer,rate=0.4,training=train)
+        layer = tf.layers.batch_normalization(layer)
+        #layer = tf.layers.dense(layer, 132, activation = tf.nn.relu)
+        #layer = tf.layers.dropout(layer,rate=0.4,training=train)
+        #layer = tf.layers.batch_normalization(layer)
+        G_prob = tf.layers.dense(layer, 134, activation = tf.nn.sigmoid)
+        tf.summary.histogram("g_3_layer", layer)
+        g_theta = get_variables(scope)
     # if sess != None:
     #     G_prob_arr = G_prob.eval(session=sess)
     #     for i in range(len(G_prob)):
     #         G_prob_arr[i] = decode_line_light(G_prob[i])
     #     G_prob = tf.Constant(G_prob_arr)
-    return G_prob
+    return g_theta, G_prob
 
-def discriminator(x):
+def discriminator(x, reuse):
+    with tf.variable_scope("discriminator", reuse=reuse) as scope:
+        #x = tf.layers.batch_normalization(x)
+        #layer = tf.layers.dense(x, 134, activation = tf.nn.relu)
+        #layer = tf.layers.dropout(layer,rate=0.4,training=train)
+        #layer = tf.layers.batch_normalization(layer)
+        layer = tf.layers.dense(x, 256, activation = activation_function)
+        tf.summary.histogram("d_1_layer", layer)
+        layer = tf.layers.dropout(layer,rate=0.4,training=train)
+        layer = tf.layers.batch_normalization(layer)
+        layer = tf.layers.dense(layer, 128, activation = activation_function)
+        tf.summary.histogram("d_2_layer", layer)
+        layer = tf.layers.dropout(layer,rate=0.4,training=train)
+        layer = tf.layers.batch_normalization(layer)
+        layer = tf.layers.dense(layer, 16, activation = tf.nn.relu)
+        tf.summary.histogram("d_3_layer", layer)
+        D_logit = tf.layers.dense(layer, 1, activation = None)
 
-    x = tf.layers.batch_normalization(x)
-    #layer = tf.layers.dense(x, 134, activation = tf.nn.relu)
-    #layer = tf.layers.dropout(layer,rate=0.4,training=train)
-    #layer = tf.layers.batch_normalization(layer)
-    layer = tf.layers.dense(x, 256, activation = activation_function)
-    layer = tf.layers.dropout(layer,rate=0.4,training=train)
-    layer = tf.layers.batch_normalization(layer)
-    layer = tf.layers.dense(layer, 128, activation = activation_function)
-    layer = tf.layers.dropout(layer,rate=0.4,training=train)
-    layer = tf.layers.batch_normalization(layer)
-    #layer = tf.layers.dense(layer, 16, activation = tf.nn.relu)
-    D_logit = tf.layers.dense(layer, 1, activation = None)
+        #D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
+        #D_logit = tf.matmul(layer, D_W2) + D_b2
+        D_prob = tf.nn.sigmoid(D_logit)
+        D_theta = get_variables(scope)
 
-    #D_h1 = tf.nn.relu(tf.matmul(x, D_W1) + D_b1)
-    #D_logit = tf.matmul(layer, D_W2) + D_b2
-    D_prob = tf.nn.sigmoid(D_logit)
-
-    return D_prob, D_logit
+    return D_theta, D_prob, D_logit
 
 
-G_sample = generator(Z)
-D_real, D_logit_real = discriminator(X)
-D_fake, D_logit_fake = discriminator(G_sample)
+G_theta, G_sample = generator(Z)
+D_theta, D_real, D_logit_real = discriminator(X, reuse=None)
+D_theta, D_fake, D_logit_fake = discriminator(G_sample, reuse=True)
 
-""" FÜR GLEICHEN DISCRIMINATOR
-G_sample = generator(Z)
+# für gleichen discriminator
+"""
+G_theta, G_sample = generator(Z)
 discriminator_temp = tf.make_template("discriminator", discriminator)
-D_real, D_logit_real = discriminator_temp(X)
-D_fake, D_logit_fake = discriminator_temp(G_sample)"""
+D_theta, D_real, D_logit_real = discriminator_temp(X)
+D_theta, D_fake, D_logit_fake = discriminator_temp(G_sample)
 # D_loss = -tf.reduce_mean(tf.log(D_real) + tf.log(1. - D_fake))
 # G_loss = -tf.reduce_mean(tf.log(D_fake))
-
+"""
 # Alternative losses:
 # -------------------
-
 
 D_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_real, labels= soft_labels(tf.shape(D_logit_real), True))) #tf.ones_like(D_logit_real)))
 D_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels= soft_labels(tf.shape(D_logit_fake), False))) #tf.zeros_like(D_logit_fake)))
 D_loss = D_loss_real + D_loss_fake
 G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logit_fake, labels=soft_labels(tf.shape(D_logit_fake), True)))# tf.ones_like(D_logit_fake)))
 
-D_solver = tf.train.GradientDescentOptimizer(0.05).minimize(D_loss)#, var_list=theta_D)
-G_solver = tf.train.AdamOptimizer(0.01).minimize(G_loss)#, var_list=theta_G)
+#G_gradients = tf.train.AdamOptimizer(0.01).compute_gradients(G_loss, var_list=G_theta)
+D_solver = tf.train.GradientDescentOptimizer(0.01).minimize(D_loss, var_list=D_theta)
+G_solver = tf.train.AdamOptimizer(0.01).minimize(G_loss, var_list=G_theta)
 
 """
 D_loss_real = tf.log(D_real)
 D_loss_fake = tf.log(tf.subtract(tf.ones_like(D_fake), D_fake))
 D_loss = - (tf.reduce_mean(tf.add(D_loss_real, D_loss_fake)))
 G_loss = - tf.reduce_mean(tf.log(D_fake))
-D_solver = tf.train.GradientDescentOptimizer(0.3).minimize(D_loss)#, var_list=theta_D)
-G_solver = tf.train.AdamOptimizer().minimize(G_loss)#, var_list=theta_G)
+D_solver = tf.train.GradientDescentOptimizer(0.03).minimize(D_loss, var_list=D_theta)
+G_solver = tf.train.AdamOptimizer(0.05).minimize(G_loss, var_list=G_theta)
 """
-
 """if sess != None:
     print("d_real. ", D_loss_real.eval(session=sess))
     print("d_fake. ", D_loss_fake.eval(session=sess))
     print("g: ", G_loss.eval(session=sess))"""
 
+tf.summary.scalar("D_loss_real", D_loss)
+tf.summary.scalar("D_loss_fake", D_loss)
+tf.summary.scalar("D_loss", D_loss)
+tf.summary.scalar("G_loss", G_loss)
 
-sess = tf.Session()
+merged = tf.summary.merge_all()
+
+train_writer = tf.summary.FileWriter("./logs/nn_logs" + '/train', sess.graph)
+
+#sess = tf.Session()
 sess.run(tf.global_variables_initializer())
+
+
+#test_writer = tf.summary.FileWriter("./logs/nn_logs" + '/test')
 
 if not os.path.exists('out/'):
     os.makedirs('out/')
@@ -180,25 +202,31 @@ i = 0
 
 d_l = list()
 g_l = list()
+gr = list()
 D_loss_curr = 0.0
+G_loss_curr= 0.0
 bar = progressbar.ProgressBar()
-for it in bar(range(600)):
+for it in bar(range(200)):
     train = True
     X_mb = preprocess_data_shuffle(mb_size).__next__()
-    #if it%5==0: improves the discriminator a lot
+    #if it%5==0: #improves the discriminator a lot
     #z = sample_Z(mb_size, Z_dim) # using same z for both doesn't change anything
+    #if G_loss_curr < 0.5:
+    summary, _, D_loss_curr = sess.run([merged, D_solver, D_loss], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
 
-    _, D_loss_curr = sess.run([D_solver, D_loss], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
-    _, G_loss_curr = sess.run([G_solver, G_loss], feed_dict={Z: sample_Z(mb_size, Z_dim)})
+    summary, _, G_loss_curr = sess.run([merged, G_solver, G_loss], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
 
+    train_writer.add_summary(summary, it)
+    #gradients = sess.run([G_gradients], feed_dict={X: X_mb, Z: sample_Z(mb_size, Z_dim)})
+    #gr.append(gradients)
     #if it % 10000 == 0:
      #   print('Iter: {}'.format(it))
       #  print('D loss: {:.4}'. format(D_loss_curr))
        # print('G_loss: {:.4}'.format(G_loss_curr))
         #print()
-    #if it%5:
-    g_l.append(G_loss_curr)
+    #if it%5==0:
     d_l.append(D_loss_curr)
+    g_l.append(G_loss_curr)
 
 print('Iter: {}'.format(it))
 print('D loss: {:.4}'. format(D_loss_curr))
@@ -206,6 +234,9 @@ print('G_loss: {:.4}'.format(G_loss_curr))
 print()
 
 
+#print("dis: ", d_l)
+#print("gen: ", g_l)
+#print(gr)
 
 train = False
 
